07bbf20233706ea74db4b829bb4d16a5728f6d8d
Louiefigz <figueroa.aaron@gmail.com>
2017-06-27 09:51:55 -0400

    adding submit button to App.js

--- App.js
+++ App.js
@@ -1,25 +1,76 @@
 import React from 'react';
-import { ScrollView, Text, StyleSheet, View, TextInput } from 'react-native';
+import { ScrollView, Text, StyleSheet, View, TextInput, ListView } from 'react-native';
+
+
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state={
+      text:'',
+      restaurants:[],
+      dataSource: ds.cloneWithRows([
+           'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+         ])
+    }
+
+    // in your constructor method
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+
+
+  }
+
+  // componentWillMount(){
+  //   const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  //   debugger
+  //   this.setState({
+  //     dataSource: ds.cloneWithRows(this.props.text)
+  //   })
+  // }
+
+  // componentDidMount(){
+  //   debugger
+  // }
+
+
+
+  onSubmitHandler(e){
+    // Make a state change here 
+
+  }
+
+
   render() {
 
     return (
       <View>
+
+
+
         <TextInput
           style={styles.input}
           placeholder="Feed Me"
           onChangeText={(text) => this.setState({text})}
+          onSubmitEditing={this.onSubmitHandler}
         />
+
+
+
         <View style={styles.background}>
 
-          <ScrollView >
-            {this.props.text.map((item)=>
-                <Text style={styles.font}>{item}</Text>
-            )}
-          </ScrollView>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => <Text style={styles.font}>{rowData}</Text>}
+          />
+
+
+
         </View>
       </View>
+
     );
   }
 }
@@ -27,7 +78,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   background:{
     alignItems: 'center'
-
   },
   font:{
     fontSize: 30,
@@ -37,6 +87,9 @@ const styles = StyleSheet.create({
   }
 })
 
+
+
 App.defaultProps = {
-  text: ['Burger King', 'McDonalds', 'Red Lobster', 'Dennys', 'Wendys', 'White Castle', 'popeyes']
+  text: ['Burger King', 'McDonalds', 'Red Lobster',
+  'Dennys', 'Wendys', 'White Castle', 'popeyes']
 }
